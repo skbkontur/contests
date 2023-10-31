22268cda03137d520f7c70de61b47fd27cf75e39
Borovik Anton <nambocovi@gmail.com>
2023-10-31 17:47:57 +0300

    fix

--- cs_schools_oct_23/jury_solutions/a.cs
+++ cs_schools_oct_23/jury_solutions/a.cs
@@ -2,28 +2,28 @@ using System;
 using System.Linq;
 
 class Program
-{
-    public static void Main()
     {
-        var sizes = Console.ReadLine().Split().Select(int.Parse).ToArray();
-        var result = 0;
-        
-        // Сгибаем лист строго по клеточкам и пополам по первому направлению
-        // Это можно делать до тех пор, пока количество клеток делится на 2
-        while (sizes[0] % 2 == 0)
+        static void Main()
         {
-            result++;
-            sizes[0] /= 2;
-        }
+            var sizes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var result = 0;
+            
+            // Сгибаем лист строго по клеточкам и пополам по первому направлению
+            // Это можно делать до тех пор, пока количество клеток делится на 2
+            while (sizes[0] % 2 == 0)
+            {
+                result++;
+                sizes[0] /= 2;
+            }
 
-        // Сгибаем лист строго по клеточкам и пополам по второму направлению
-        // Это можно делать до тех пор, пока количество клеток делится на 2
-        while (sizes[1] % 2 == 0)
-        {
-            result++;
-            sizes[1] /= 2;
-        }
+            // Сгибаем лист строго по клеточкам и пополам по второму направлению
+            // Это можно делать до тех пор, пока количество клеток делится на 2
+            while (sizes[1] % 2 == 0)
+            {
+                result++;
+                sizes[1] /= 2;
+            }
 
-        Console.WriteLine(result);
+            Console.WriteLine(result);
+        }
     }
-}
